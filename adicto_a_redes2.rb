ad87a4260a2bc06da25f4b12a2fa9384e211907d
jackypy <jacqueline.parra7@gmail.com>
2019-04-14 23:40:54 -0400

    adictos_a_redes_v2

---
+++ adicto_a_redes2.rb
@@ -0,0 +1,17 @@
+def scan_addicts2(array)
+  array = [120, 50, 600, 30, 90, 10, 200, 0, 500]
+  results = []
+  n = array.count
+  n.times do |i|
+if array[i] < 90
+  results.push 'bien'
+elsif array[i] >= 180
+  results.push 'mal'
+else
+  results.push 'mejorable'
+    end
+  end
+  print results
+end
+
+print scan_addicts2(Array)
